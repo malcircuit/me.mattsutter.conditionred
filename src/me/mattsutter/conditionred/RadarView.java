a7563c3a48cd76eba112973f7b0c246881f48e25
Matt Sutter <matt@mattsutter.me>
2013-01-25 12:10:29 -0600

    Consolidate UI mess into single RadarView class.

--- src/me/mattsutter/conditionred/RadarView.java
+++ src/me/mattsutter/conditionred/RadarView.java
@@ -1,57 +1,72 @@
 package me.mattsutter.conditionred;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import me.mattsutter.conditionred.util.RenderCommand;
-
 import android.content.Context;
-import android.graphics.PixelFormat;
-import android.opengl.GLSurfaceView;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class RadarView extends GLSurfaceView {
+public class CustomMapView extends View {
+	
+	public static final int METERS_PER_NMI = 1852;
+	//public static final float RADAR_MAP_D = 248/60;
 
-	protected final RadarRenderer renderer;
-//	private final RenderCommandQueue queue = new RenderCommandQueue();
-//	private int prod_code;
+	// Possible radii for radial products in meters.
+	public static final int SHORT_DIAM = 64 * METERS_PER_NMI;
+	public static final int DIAMETER = 248 * METERS_PER_NMI;
+	public static final int LONG_DIAM = 496 * METERS_PER_NMI;
+	public static final int VEL_DIAM = 324 * METERS_PER_NMI;
+	public static final int ECHO_TOP_DIAM = 372 * METERS_PER_NMI;
+	
+	private GestureDetector gest_detect;
+	
+	private Runnable progOn, progOff;
+	private Handler handler;
+	private boolean progress = false;
 
-	public RadarView(Context context, ConcurrentLinkedQueue<RenderCommand> queue, int frame_num) {
+	public CustomMapView(Context context) {
 		super(context);
-		renderer = new RadarRenderer(queue, frame_num);
-
-		setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-		setRenderer(renderer);
-//		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-		setDebugFlags(GLSurfaceView.DEBUG_CHECK_GL_ERROR);
-		getHolder().setFormat(PixelFormat.TRANSLUCENT);
-		setZOrderOnTop(true);
+		init(context);
+	}
+	
+	private void init(Context context){
+		gest_detect = new GestureDetector(context, (GestureDetector.OnGestureListener) context);
+		gest_detect.setOnDoubleTapListener((GestureDetector.OnDoubleTapListener) context);
 	}
 
-//	public void newRadarImage(RadialProduct product){
-////		if (product != null){
-////			prod_code = product.prod_type;
-////			mapHasChanged(product.prod_block.radar_center);
-////			return queue.sendProductChange(product);
-////		}
-////		return false;
-//		
-//		if (product != null){
-//			prod_code = product.prod_type;
-//			mapHasChanged(product.prod_block.radar_center);
-//			queue.add(new RenderCommand(product));
-//		}
-//	}
-//	
-//	protected void mapHasChanged(GeoPoint radar_center){
-////		return queue.sendMapUpdate(map_view, prod_code, radar_center);
-//		queue.add(new RenderCommand(map_view, prod_code, radar_center));
-//	}
-//	
-//	protected void changeAlpha(short alpha){
-////		return queue.sendAlphaChange(alpha);
-//		queue.add(new RenderCommand(alpha));
-//	}
+	@Override
+	public boolean onTouchEvent(MotionEvent e){
+		if (gest_detect.onTouchEvent(e))
+			return true;
+		return super.onTouchEvent(e);
+	}
+
+	protected void setProgRunners(Handler hand, Runnable progOn, Runnable progOff){
+		this.progOn = progOn;
+		this.progOff = progOff;
+		handler = hand;
+	}
 	
-	public void onDestroy(){
-		renderer.deInitAnimation();
+	public void progressOn(){
+		if (progOn != null)
+			handler.post(progOn);
+	}
+	
+	public void progressOff(){
+		if (progOff != null)
+			handler.post(progOff);
+	}
+	
+	protected boolean toggleProgress(){
+		if (progress){
+			progress = false;
+			progressOff();
+		}
+		else{
+			progress = true;
+			progressOn();
+		}
+		
+		return progress;
 	}
 }
